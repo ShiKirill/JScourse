49bb4050bca963dc959569e452748b830f90d0f2
ShiKirill <shiraevkirill@gmail.com>
2021-01-29 15:26:44 +0700

    Second lesson

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const money = 0;
-const income = 0;
-const addExpenses = 0;
-const deposit = 0;
-const mission = '';
-const period = '';
+const money = 10000;
+const income = 'Фриланс';
+const addExpenses = 'Такси, одежда, кафе';
+const deposit = true;
+const mission = 100000;
+const period = 12;
 
-alert('Good day!');
-console.log('Hello world!');
+// alert('Good day!');
+// console.log('Hello world!');
+
+console.log('Money: ', typeof money);
+console.log('Income: ', typeof income);
+console.log('Deposit: ',typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} долларов`);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+const budgetDay = money / 30;
+console.log(budgetDay.toFixed(2));
