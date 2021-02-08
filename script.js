03e17ab0344837303708d08f7022e824643fe1ab
ShiKirill <shiraevkirill@gmail.com>
2021-02-08 19:52:09 +0700

    Revert "Lesson function capitalizeFirstLetter(string) {"
    This reverts commit 6ea4155280330fc6a0285ba5c0d6e33933526605.

--- script.js
+++ script.js
@@ -24,33 +24,16 @@ let appData ={
  budgetMonth: 0,
  expensesMonth: 0,
  deposit: false,
- percentDeposit: 0,
- moneyDeposit: 0,
  mission: 100000,
  period: 6,
  budget: money,
  asking: function(){
-  if (confirm('Есть ли у вас дополнительный источник заработка?')){
-   let itemIncome;
-   let cashIncome;
-   do{
-   itemIncome = prompt('Какой у вас дополнительный источник заработка?');
-   } while(!isNaN(parseFloat(itemIncome)));
-   do{
-   cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?');
-  } while(!isNumber(cashIncome));
-   appData.income[itemIncome] = cashIncome;
-  }
-
   const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
   appData.addExpenses = addExpenses.toLowerCase().split(', ');
-  appData.deposit = confirm('Есть ли у вас депозит в банке?');
+  const deposit = confirm('Есть ли у вас депозит в банке?');
   let tmpValue;
   for (let i=0; i<2; i++ ){
-   let tmpExpense;
-   do{
-   tmpExpense = prompt('Введите обязательную статью расходов');
-   } while(!isNaN(parseFloat(tmpExpense)));
+   let tmpExpense = prompt('Введите обязательную статью расходов');
    do{
     tmpValue = prompt('Во сколько это обойдётся?');
    } while (!isNumber(tmpValue));
@@ -67,7 +50,7 @@ let appData ={
   appData.budgetDay = Math.floor(appData.budgetMonth/30);
  },
  getTargetMonth:  function(){
-  return Math.ceil(appData.mission/appData.budgetMonth);
+  appData.period = Math.ceil(appData.mission/appData.budgetMonth);
  },
  getStatusIncome: function(){
   if (appData.budgetDay >= 1200) { return ('У вас высокий уровень дохода');
@@ -75,31 +58,18 @@ let appData ={
   } else if (appData.budgetDay>=0 && appData.budgetDay<600) {return ('К сожалению у вас уровень дохода ниже среднего');
   } else {return ('Что то пошло не так');}
  },
- getInfoDeposit: function(){
-  if (appData.deposit){
-   do{
-   appData.percentDeposit = prompt('Какой годовой процент?');
-   } while(!isNumber(appData.percentDeposit));
-   do{
-   appData.moneyDeposit = prompt('Какая сумма заложена?');
-   } while(!isNumber(appData.moneyDeposit));
-  }
- },
- calcSavedMoney: function(){
-  return appData.budgetMonth * appData.period;
- }
 };
 
 appData.asking();
 appData.getExpensesMonth();
 appData.getBudget();
-appData.getInfoDeposit();
+appData.getTargetMonth();
 
 console.log('Расходы за месяц: ' + appData.expensesMonth);
-if (appData.getTargetMonth()<0) {
+if (appData.period<0) {
 console.log('Цель не будет достигнута');
 } else {
-console.log(`Цель будет достигнута за ${appData.getTargetMonth()} месяцев(-а)`);
+console.log(`Цель будет достигнута за ${appData.period} месяцев(-а)`);
 }
 console.log(appData.getStatusIncome());
 
@@ -107,11 +77,3 @@ console.log('Наша программа включает:');
 for (let key in appData){
  console.log(key + ': ' + appData[key]);
 }
-
-function capitalizeFirstLetter(string) {
- return string.charAt(0).toUpperCase() + string.slice(1);
-}
-for (let i =0;i<appData.addExpenses.length;i++){
- appData.addExpenses[i] = capitalizeFirstLetter(appData.addExpenses[i]);
-}
-console.log(appData.addExpenses.join(', '));
